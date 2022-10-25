7f14521ea9e61de4a379d1eae65746caa92edcd7
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-10-25 20:41:50 +0600

    navigation bar added

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Courses = () => {
+    return (
+        <div className='grid grid-cols-5'>
+            <h1 className='col-span-2'>Sidebar</h1>
+            <p>Body</p>
+        </div>
+    );
+};
+
+export default Courses;
